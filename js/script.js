91491997271d21c04f5cf52f10152a47c6edce17
Kaminishikawahara KK <kkaori146@gmail.com>
2021-08-03 10:12:29 +0900

    First Step: Script Structure

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-alert('Olá')
+function tabuada () {
+    let num = document. getElementById('txtn')
+    let tab = document.getElementById('seltab')
+
+    if(num.value.length == 0) {
+        window.alert('Por favor digite um número!')
+        
+    } else {
+        let n = Number(num.value)
+        let c = 1
+        while ( c <= 10) {
+            let item = document.createElement('option')
+            item.text = `${num} X ${c} = ${num*c}`
+            tab.appendChild(item)
+        }
+    }
+}
